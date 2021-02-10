6a86ddf8c5dfb52384184b32a5a67a39b89aaee4
Prateek Kumar <prateekkumartiwari01@gmail.com>
2021-02-11 02:36:36 +0530

    master

---
+++ index.js
@@ -0,0 +1,63 @@
+const Express = require("express");
+const fs = require("fs");
+const app = new Express();
+
+
+app.get("/",(req, res) => {
+    res.sendFile(__dirname+"/index.html");
+});
+
+/*
+Request Header {
+    Request-URL : "http://localhost:8080/video"
+    Request Method : "GET"
+    Range : bytes 0 -  
+}
+
+Multiple Such Requests will be made by the web browser while streaming the video.
+But everytime value of range is gonna increase and initial value should be 0 as it is starting from start.
+
+
+Response Header {
+    Content-Range:bytes 0-1000001/totalvideosize,
+    Accept-Range:bytes,
+    Content-Length:1000001 (here 1 MB , but can be changed),
+    Content-Type:video/mp4,
+}
+*/
+
+app.get("/video",(req, res) => {
+    const range = req.headers.range;
+    if(!range){
+        res.status(400).send("Needs Range In Header");
+    }
+    //Range = bytes=1234-
+    const videoPath = "NodeVideo.mp4";
+    const videoSize = fs.statSync("NodeVideo.mp4").size;
+
+    const chunkSize =  10 ** 6; //1 MB
+
+    //var name Should be "start"
+    const start = Number(range.replace(/\D/g,""));
+    //Here we remove "-" from the range.
+    //var name should be end and end will have min of either two and it's used to stop video streaming when we reach max video size
+    const end = Math.min(chunkSize+start,videoSize-1);
+    const contentLength = end - start + 1 ;
+
+    const headers = {
+        "Content-Range":`bytes ${start}-${end}/${videoSize}`,
+        "Accept-Range":"bytes",
+        "Content-Length":contentLength,
+        "Content-Type":"video/mp4",
+    };
+    //Response Code 206 is for Partial Content.
+    res.writeHead(206,headers);
+    const videoStream = fs.createReadStream(videoPath,{start ,end});
+    /*Pipe basically allows us to plug stream together passing data from one stream to another
+     without having to worry about events as streams are based on events.
+    */
+    videoStream.pipe(res);
+});
+
+app.listen(8080,()=>console.log("Server Running at Port 8080"));
+
